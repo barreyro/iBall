dc6710cf8f90c1b38b4da0c9e4950bb394a251e9
Stephy B <ssbarreyro@ucdavis.edu>
2015-01-07 15:17:59 -0500

    Fix typo in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.create(username: "KD35", email: "k@k.com", password: "k", first_name: "Kevin", last_name: "Durant", age: 26, height: 83, gender: "dude", bio: "OKC"}
-User.create(username: "SwaggyP", email: "s@s.com", password: "s", first_name: "Nick", last_name: "Young", age: 26, height: 81, gender: "dude", bio: "LAL"}
-User.create(username: "Chef Stef", email: "c@c.com", password: "c", first_name: "Stephen", last_name: "Curry", age: 26, height: 81, gender: "dude", bio: "GSW"}
-User.create(username: "K.Love", email: "kl@kl.com", password: "k", first_name: "Kevin", last_name: "Love", age: 26, height: 82, gender: "dude", bio: "CLC"}
-User.create(username: "Black Mamba", email: "b@b.com", password: "b", first_name: "Kobe", last_name: "Bryant", age: 36, height: 78, gender: "dude", bio: "LAL"}
+User.create(username: "KD35", email: "k@k.com", password: "k", first_name: "Kevin", last_name: "Durant", age: 26, height: 83, gender: "dude", bio: "OKC")
+User.create(username: "SwaggyP", email: "s@s.com", password: "s", first_name: "Nick", last_name: "Young", age: 26, height: 81, gender: "dude", bio: "LAL")
+User.create(username: "Chef Stef", email: "c@c.com", password: "c", first_name: "Stephen", last_name: "Curry", age: 26, height: 81, gender: "dude", bio: "GSW")
+User.create(username: "K.Love", email: "kl@kl.com", password: "k", first_name: "Kevin", last_name: "Love", age: 26, height: 82, gender: "dude", bio: "CLC")
+User.create(username: "Black Mamba", email: "b@b.com", password: "b", first_name: "Kobe", last_name: "Bryant", age: 36, height: 78, gender: "dude", bio: "LAL")
