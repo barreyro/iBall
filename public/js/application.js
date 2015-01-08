b05795a9990a539a5b043c702cb276778b95764c
Stephy B <ssbarreyro@ucdavis.edu>
2015-01-08 02:06:17 -0500

    Cleaned JS code.

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $("body").on("click", "#signup", function(event){
     event.preventDefault();
-    $target = $(event.target);
+    var $target = $(event.target);
     $.ajax({
       type: "GET",
       url: "/signup",
     }).done(function(response){
       $(".container").replaceWith(response);
-    })
-  })
+    });
+  });
 
   $("body").on("click", "#login", function(event){
     event.preventDefault();
@@ -18,35 +18,35 @@ $(document).ready(function() {
       url: "/",
     }).done(function(response){
       $(".container").replaceWith(response);
-    })
-  })
+    });
+  });
 
   $(".edit_comment_link").click(function(event){
     var $this = $(this)
     event.preventDefault();
     $this.hide();
     $this.siblings(".edit_form_div").css('display','block');
-  })
+  });
 
   $(".add_comment_button").click(function(event){
     event.preventDefault();
-    $target = $(event.target);
+    var $target = $(event.target);
     $target.hide();
     $(".comment_form").css('display','block');
-  })
+  });
 
  $('#comment_block').on('submit', '.comment_form form', function(event) {
     event.preventDefault()
-    $form = $(this)
+    var $form = $(this)
     console.log($form)
     $.ajax({
       type: "POST",
       url: $form.attr('action'),
-      data:  $form.serialize()
+      data: $form.serialize()
     }).done(function(response){
       console.log(response);
       $('.userComments').append(response);
-    })
-  })
+    });
+  });
 
 });
